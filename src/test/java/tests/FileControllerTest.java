5a04e1b272e13cf16595ec888d8db06eaf65a83b
alu0100303612 <alu0100303612@ull.edu.es>
2016-07-05 14:53:59 +0100

    Modificadas las pruebas, para que sigan el estilo de JUnit4

--- src/test/java/tests/FileControllerTest.java
+++ src/test/java/tests/FileControllerTest.java
@@ -1,7 +1,9 @@
 package tests;
 import java.io.File;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import app.FileController;
@@ -12,16 +14,37 @@ import app.FileController;
  *
  */
 public class FileControllerTest {
+	FileController fileController;
+	
+	/**
+	 * Función que se ejecuta antes de testFileController(). Crea el objeto con el que se harán las pruebas
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		fileController = new FileController();
+	}
 
+	/**
+	 * Función que se ejecuta después de testFileController. Establece fileController como Null
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		fileController = null;
+	}
+	
     /**
      * Test que comprueba que la funcion fileToString(File) devuelve el valor 
      * esperado.
      */
     @Test
     public void testFileController() {
-        FileController fileController = new FileController();
+        Assert.assertNotNull(fileController);
+        
         String expectedText = "This is the file number one ";
         File file = new File("inputFiles/file1.txt");
+        Assert.assertNotNull(file);
         String text = fileController.fileToString(file);
         Assert.assertEquals(expectedText, text);
     }
--- src/test/java/tests/LoadFilesTest.java
+++ src/test/java/tests/LoadFilesTest.java
@@ -1,23 +1,55 @@
 package tests;
 
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import app.LoadFiles;
-import junit.framework.TestCase;
 
 /**
  * Clase que implementa tests para la clase LoadFiles
  * @author Bianney
  *
  */
-public class LoadFilesTest extends TestCase {
+public class LoadFilesTest{
+	static String[] filesName;
+	
+	/**
+	 * Función que se ejecuta antes de los tests. Carga en la variable filesName el nombre de los ficheros
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		filesName = LoadFiles.load();
+	}
 
-   /**
+	/**
+	 * Función que se ejecuta después de los tests. Asigna el valor null a filesName
+	 * @throws Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		filesName = null;
+	}
+	
+	/**
+     * Test que comprueba que se puede acceder a un método de la clase LoadFiles
+     */
+	@Test
+    public void testAccessLoadFiles(){
+        assertNotNull("No se puede acceder correctamente", filesName);
+    }
+	
+	/**
      * Test que comprueba el buen funcionamiento de la funcion load()
      */
+	@Test
     public void testLoadFiles(){
         //Array con los nombres esperados de los ficheros
         String[] filesNameExpected = new String[4];
@@ -26,15 +58,7 @@ public class LoadFilesTest extends TestCase {
         filesNameExpected[2] = "file3.txt";
         filesNameExpected[3] = "file4.txt";
         //Array con los nombres obtenidos con la funcion load()
-        String[] filesName = LoadFiles.load();
         Arrays.sort(filesName);
         Assert.assertArrayEquals(filesNameExpected, filesName);
-    }
-
-    /**
-     * Test que comprueba que se puede acceder a un método de la clase LoadFiles
-     */
-    public void testAccessLoadFiles(){
-        assertNotNull("Se puede acceder correctamente", LoadFiles.load());
-    }
+	}
 }
